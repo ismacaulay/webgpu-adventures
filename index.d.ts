9422ea753eae391c8716087e24466defd16bcb1e
Ian Macaulay <ismacaul@gmail.com>
2020-07-02 15:09:13 -0600

    Convert from rollup to webpack
    For some reason I find webpack better, probably need to read the rollup
    docs

--- index.d.ts
+++ index.d.ts
@@ -7,3 +7,13 @@ declare module '*.frag' {
     const value: string;
     export default value;
 }
+
+declare module '*.svelte' {
+    const value: any;
+    export default value;
+}
+
+declare module '*glslang.js' {
+    const value: any;
+    export default value;
+}
