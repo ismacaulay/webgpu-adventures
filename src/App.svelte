401f73e406ca0733da69524539a2e7dc278abc8b
Ian Macaulay <ismacaul@gmail.com>
2022-02-13 12:35:45 -0700

    Removed nav and put it just on the home page

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,7 @@
 <script>
   import router from 'page';
   import Home from './pages/Home.svelte';
-  import HelloWorld from './pages/hello-world/Page.svelte';
-  // import ECS from './pages/ecs/Page.svelte';
-  // import Cube from './pages/learn-opengl/cube/Page.svelte';
-  // import RotatingCube from './pages/learn-opengl/rotating-cube/Page.svelte';
-  // import Lighting from './pages/learn-opengl/lighting/Page.svelte';
-  // import LightingMaps from './pages/learn-opengl/lighting-maps/Page.svelte';
-  // import LightCasters from './pages/learn-opengl/light-casters/Page.svelte';
-  // import MultipleLights from './pages/learn-opengl/multiple-lights/Page.svelte';
-  // import DepthTesting from './pages/learn-opengl/depth-testing/Page.svelte';
-  // import StencilTesting from './pages/learn-opengl/stencil-testing/Page.svelte';
-
-  const examples = [
-    { title: 'hello-world', component: HelloWorld },
-    // { title: 'ecs', component: ECS },
-    // { title: 'learn-opengl/basics', component: Cube },
-    // { title: 'learn-opengl/transformations', component: RotatingCube },
-    // { title: 'learn-opengl/materials', component: Lighting },
-    // { title: 'learn-opengl/lighting-maps', component: LightingMaps },
-    // { title: 'learn-opengl/light-casters', component: LightCasters },
-    // { title: 'learn-opengl/multiple-lights', component: MultipleLights },
-    // { title: 'learn-opengl/depth-testing', component: DepthTesting },
-    // { title: 'learn-opengl/stencil-testing', component: StencilTesting },
-  ];
+  import examples from './pages/routes';
 
   let page;
   router('/', () => (page = Home));
@@ -36,63 +14,18 @@
 </script>
 
 <style>
-  main {
-    display: flex;
-    flex-direction: row;
-    width: 100vw;
-  }
-
-  h1 {
-    font-size: 14pt;
-    text-align: center;
-    margin-bottom: 40px;
-  }
-
-  #nav {
-    position: relative;
-    left: 0px;
-    flex: 1;
-    max-width: 300px;
-    min-width: 300px;
-    height: 100vh;
-    overflow: auto;
-    background: #ebebeb;
-  }
-
-  .nav-link {
-    display: block;
-    padding: 0 10px;
-  }
-
+  /* main { */
+  /*   display: flex; */
+  /*   flex-direction: row; */
+  /*   width: 100vw; */
+  /* } */
   #panel {
     height: 100vh;
     width: 100%;
     overflow: auto;
   }
-
-  a {
-    text-decoration: none;
-  }
-
-  a:link,
-  a:visited {
-    color: #357785;
-  }
-  a:hover {
-    text-decoration: underline;
-  }
 </style>
 
-<main>
-  <div id="nav">
-    <h1>
-      <a href="/">WebGPU Adventures</a>
-    </h1>
-    {#each examples as example}
-      <a class="nav-link" href="/{example.title}">{example.title}</a>
-    {/each}
-  </div>
-  <div id="panel">
-    <svelte:component this={page} />
-  </div>
-</main>
+<div id="panel">
+  <svelte:component this={page} />
+</div>
---
+++ src/pages/routes.ts
@@ -0,0 +1,23 @@
+import HelloWorld from './hello-world/Page.svelte';
+// import ECS from './pages/ecs/Page.svelte';
+// import Cube from './pages/learn-opengl/cube/Page.svelte';
+// import RotatingCube from './pages/learn-opengl/rotating-cube/Page.svelte';
+// import Lighting from './pages/learn-opengl/lighting/Page.svelte';
+// import LightingMaps from './pages/learn-opengl/lighting-maps/Page.svelte';
+// import LightCasters from './pages/learn-opengl/light-casters/Page.svelte';
+// import MultipleLights from './pages/learn-opengl/multiple-lights/Page.svelte';
+// import DepthTesting from './pages/learn-opengl/depth-testing/Page.svelte';
+// import StencilTesting from './pages/learn-opengl/stencil-testing/Page.svelte';
+
+export default [
+  { title: 'hello-world', component: HelloWorld },
+  // { title: 'ecs', component: ECS },
+  // { title: 'learn-opengl/basics', component: Cube },
+  // { title: 'learn-opengl/transformations', component: RotatingCube },
+  // { title: 'learn-opengl/materials', component: Lighting },
+  // { title: 'learn-opengl/lighting-maps', component: LightingMaps },
+  // { title: 'learn-opengl/light-casters', component: LightCasters },
+  // { title: 'learn-opengl/multiple-lights', component: MultipleLights },
+  // { title: 'learn-opengl/depth-testing', component: DepthTesting },
+  // { title: 'learn-opengl/stencil-testing', component: StencilTesting },
+];
