f0ba07e22b37b7a43dfdbbcd13c8f69413bff8d2
Ian Macaulay <ismacaul@gmail.com>
2022-02-14 19:28:46 -0700

    Fix file names

--- src/pages/examples.ts
+++ src/pages/examples.ts
@@ -1,9 +1,8 @@
 import HelloWorld from './hello-world/Page.svelte';
 import Cube from './cube/Page.svelte';
 // import ECS from './ecs/Page.svelte';
-import TexturedCube from './learn-opengl/1-texturedCube/Page.svelte';
-// import RotatingCube from './pages/learn-opengl/rotating-cube/Page.svelte';
-// import Lighting from './pages/learn-opengl/lighting/Page.svelte';
+import TexturedCube from './learn-opengl/1-textured-cube/Page.svelte';
+// import Materials from './learn-opengl/2-materials/Page.svelte';
 // import LightingMaps from './pages/learn-opengl/lighting-maps/Page.svelte';
 // import LightCasters from './pages/learn-opengl/light-casters/Page.svelte';
 // import MultipleLights from './pages/learn-opengl/multiple-lights/Page.svelte';
@@ -13,13 +12,12 @@ import TexturedCube from './learn-opengl/1-texturedCube/Page.svelte';
 export default [
   { title: 'hello-world', component: HelloWorld },
   { title: 'cube', component: Cube },
-  // { title: 'ecs', component: ECS },
   { title: 'learn-opengl/basics', component: TexturedCube },
-  // { title: 'learn-opengl/transformations', component: RotatingCube },
-  // { title: 'learn-opengl/materials', component: Lighting },
+  // { title: 'learn-opengl/materials', component: Materials },
   // { title: 'learn-opengl/lighting-maps', component: LightingMaps },
   // { title: 'learn-opengl/light-casters', component: LightCasters },
   // { title: 'learn-opengl/multiple-lights', component: MultipleLights },
   // { title: 'learn-opengl/depth-testing', component: DepthTesting },
   // { title: 'learn-opengl/stencil-testing', component: StencilTesting },
+  // { title: 'ecs', component: ECS },
 ];
